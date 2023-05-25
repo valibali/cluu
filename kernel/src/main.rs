e58a3ded8e854cd9677d7277aa17a8c0aa6c3e85
Valkony Balazs (XC/ENG4-Bp) <Balazs.Valkony@hu.bosch.com>
2023-05-25 14:27:54 +0200

    Fix pointer sizes, linker

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,7 +1,7 @@
 /*
  * mykernel/rust/src/main.rs
  *
- * Copyright (C) 2017 - 2021 Vinay Chandra
+ * Copyright (C) 2017 - 2022 Vinay Chandra, Valkony Balázs
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -50,32 +50,33 @@ extern crate rlibc;
 fn _start() -> ! {
     /*** NOTE: this code runs on all cores in parallel ***/
     use bootboot::*;
+
+    //Lets use the BOOTBOOT_INFO as a pointer, dereference it and immediately borrow it.
     let bootboot_r = unsafe { & (*(BOOTBOOT_INFO as *const BOOTBOOT)) };
+    let fb = BOOTBOOT_FB as u64;
 
     if bootboot_r.fb_scanline > 0 {
-    
-        let fb = BOOTBOOT_FB as u64;
 
         // cross-hair to see screen dimension detected correctly
         for y in 0..bootboot_r.fb_height {
             let addr = fb
                 + bootboot_r.fb_scanline as u64 * y as u64
                 + bootboot_r.fb_width as u64 * 2;
-            unsafe { *(addr as *mut u64) = 0x00FFFFFF };
+            unsafe { *(addr as *mut u32) = 0x00FFFFFF };
         }
         for x in 0..bootboot_r.fb_width {
             let addr = fb
                 + bootboot_r.fb_scanline as u64 * (bootboot_r.fb_height / 2) as u64 + (x * 4) as u64;
-            unsafe { *(addr as *mut u64) = 0x00FFFFFF };
+            unsafe { *(addr as *mut u32) = 0x00FFFFFF };
         }
 
-        // red, green, blue boxes in order
+        //ed, green, blue boxes in order
         for y in 0..20 {
             for x in 0..20 {
                 let addr = fb
                     + bootboot_r.fb_scanline as u64 * (y + 20) as u64
                     + (x + 20) * 4;
-                unsafe { *(addr as *mut u64) = 0x00FF0000 };
+                unsafe { *(addr as *mut u32) = 0x00FF0000 };
             }
         }
         for y in 0..20 {
@@ -83,7 +84,7 @@ fn _start() -> ! {
                 let addr = fb
                     + bootboot_r.fb_scanline as u64 * (y + 20) as u64
                     + (x + 50) * 4;
-                unsafe { *(addr as *mut u64) = 0x0000FF00 };
+                unsafe { *(addr as *mut u32) = 0x0000FF00 };
             }
         }
         for y in 0..20 {
@@ -91,14 +92,14 @@ fn _start() -> ! {
                 let addr = fb
                     + bootboot_r.fb_scanline as u64 * (y + 20) as u64
                     + (x + 80) * 4;
-                unsafe { *(addr as *mut u64) = 0x000000FF };
+                unsafe { *(addr as *mut u32) = 0x000000FF };
             }
         }
     }
 
-    // say hello
+        // say hello
     puts("Hello Rust Hobby Kernel");
-    
+
     // hang for now
     loop {}
 }
@@ -106,19 +107,17 @@ fn _start() -> ! {
 /**************************
  * Display text on screen *
  **************************/
- //TODO: REFACTOR 
- 
- fn puts(string: &'static str) {
+fn puts(string: &'static str) {
     use bootboot::*;
 
-    let fb = BOOTBOOT_FB as u64;
-    let bootboot_r = unsafe { & (*(BOOTBOOT_INFO as *const BOOTBOOT)) };
+    let fb: u64 = BOOTBOOT_FB as u64;
+    let bootboot_r: &BOOTBOOT = unsafe { & (*(BOOTBOOT_INFO as *const BOOTBOOT)) };
 
     unsafe {
         let font: *mut psf2_t = &_binary_font_psf_start as *const u64 as *mut psf2_t;
         let (mut kx, mut line, mut mask, mut offs): (u32, u64, u64, u32);
         kx = 0;
-        let bpl = ((*font).width + 7) / 8;
+        let bpl: u32 = ((*font).width + 7) / 8;
 
         for s in string.bytes() {
             let glyph_a: *mut u8 = (font as u64 + (*font).headersize as u64) as *mut u8;
@@ -134,7 +133,7 @@ fn _start() -> ! {
                 line = offs as u64;
                 mask = 1 << ((*font).width - 1);
                 for _x in 0..(*font).width {
-                    let target_location = (fb as *const u8 as u64 + line) as *mut u32;
+                    let target_location: *mut u32 = (fb as *const u8 as u64 + line) as *mut u32;
                     let mut target_value: u32 = 0;
                     if (*glyph as u64) & (mask) > 0 {
                         target_value = 0xFFFFFF;
@@ -143,7 +142,7 @@ fn _start() -> ! {
                     mask >>= 1;
                     line += 4;
                 }
-                let target_location = (fb as *const u8 as u64 + line) as *mut u32;
+                let target_location: *mut u32 = (fb as *const u8 as u64 + line) as *mut u32;
                 *target_location = 0;
                 glyph = glyph.offset(bpl as isize);
                 offs += bootboot_r.fb_scanline;
