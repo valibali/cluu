1fb843ddb0b06d49f4eb6e4ab0d85e038fce685a
Valkony Balazs (XC/ENG4-Bp) <Balazs.Valkony@hu.bosch.com>
2023-06-13 14:17:37 +0200

    Implement framebuffer

--- kernel/src/arch/x86_64/peripheral/mod.rs
+++ kernel/src/arch/x86_64/peripheral/mod.rs
@@ -1,3 +1,26 @@
-pub use self::uart_16550::*;
+use core::ptr::addr_of_mut;
+
+use syscall::pio::Pio;
+use spin::Mutex;
+
+use self::uart_16550::SerialPort;
+use self::framebuffer::*;
+use crate::bootboot::*;
 
 pub mod uart_16550;
+pub mod framebuffer;
+
+
+pub static COM2: Mutex<SerialPort<Pio<u8>>> = Mutex::new(SerialPort::<Pio<u8>>::new(0x2F8));
+
+pub static FB: Mutex<Option<FrameBuffer>> = Mutex::new(None);
+
+
+pub fn init_peripherals() {
+    COM2.lock().init();
+    *FB.lock() = Some(FrameBuffer::new(
+        unsafe {addr_of_mut!(fb)} as *mut u32,
+        unsafe { bootboot.fb_scanline }, 
+        unsafe { bootboot.fb_width }, 
+        unsafe { bootboot.fb_height }))
+}
