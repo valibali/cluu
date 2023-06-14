8a15bb79db83109d0006fa019981bf1498eaa0c5
valibali <valkonybalazs@gmail.com>
2023-06-14 13:28:14 +0200

    Move uart and its abstraction to directories that make more sense

--- kernel/src/arch/x86_64/peripheral/mod.rs
+++ kernel/src/arch/x86_64/peripheral/mod.rs
@@ -1,7 +1,3 @@
-pub use self::serial::*;
+pub use self::uart_16550::*;
 
-pub mod serial;
-
-pub unsafe fn init_noncpu_perif() {
-    serial::init();
-}
+pub mod uart_16550;
--- kernel/src/peripherals/serial.rs
+++ kernel/src/peripherals/serial.rs
@@ -1,6 +1,7 @@
-use peripherals::uart_16550::SerialPort;
+use arch::x86_64::peripheral::uart_16550::SerialPort;
 use syscall::pio::Pio;
 use spin::Mutex;
+
 
 pub static COM1: Mutex<SerialPort<Pio<u8>>> = Mutex::new(SerialPort::<Pio<u8>>::new(0x3F8));
 pub static COM2: Mutex<SerialPort<Pio<u8>>> = Mutex::new(SerialPort::<Pio<u8>>::new(0x2F8));
@@ -9,3 +10,4 @@ pub unsafe fn init() {
     COM1.lock().init();
     COM2.lock().init();
 }
+
