4e2e4e884bbbc580a09367f5bb64fad3d03918a1
valibali <valkonybalazs@gmail.com>
2026-01-07 22:27:14 +0100

    fix type inference

--- userspace/libcluu/src/process.rs
+++ userspace/libcluu/src/process.rs
@@ -18,7 +18,7 @@ pub fn map_segments(space_token: usize, elf: &ElfFile, bytes: &[u8]) -> Result<(
 fn map_segment(space_token: usize, segment: &LoadableSegment, bytes: &[u8]) -> Result<()> {
     let start = segment.vaddr as usize;
     if start % PAGE_SIZE != 0 {
-        return Err(Error::InvalidArgument);
+        return Err(Error::InvalidArgument.into());
     }
 
     let mem_size = segment.mem_size as usize;
@@ -29,7 +29,7 @@ fn map_segment(space_token: usize, segment: &LoadableSegment, bytes: &[u8]) -> R
     let file_offset = segment.file_offset as usize;
     let file_size = segment.file_size as usize;
     if file_offset + file_size > bytes.len() {
-        return Err(Error::InvalidArgument);
+        return Err(Error::InvalidArgument.into());
     }
 
     let slice = &bytes[file_offset..file_offset + file_size];
