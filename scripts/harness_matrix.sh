b8199a57db85868cb38b62c64d1d13d3f670ad88
valibali <valkonybalazs@gmail.com>
2026-02-10 14:48:55 +0100

    m5: enforce fairness SLO thresholds in harness matrix

--- scripts/harness_matrix.sh
+++ scripts/harness_matrix.sh
@@ -36,7 +36,7 @@ if [[ "$RUN_BUILD_ONCE" -eq 1 ]]; then
     run_case "m4_notify_lifecycle" no_build env MARKER_MODE=m4_notify_lifecycle TEST_COMMAND_REPEAT=1 ./test_hello.sh
     run_case "m4_deny_paths" full env MARKER_MODE=m4_deny_paths TEST_COMMAND_REPEAT=1 RUN_WAIT=12 ./test_hello.sh
     run_case "m4_registry_deny_paths" full env MARKER_MODE=m4_registry_deny_paths TEST_COMMAND_REPEAT=1 RUN_WAIT=12 ./test_hello.sh
-    run_case "m5_fairness" full env MARKER_MODE=m5_fairness TEST_COMMAND_REPEAT=1 RUN_WAIT=16 MIN_EXIT_COOKIES=6 ./test_hello.sh
+    run_case "m5_fairness" full env MARKER_MODE=m5_fairness TEST_COMMAND_REPEAT=1 RUN_WAIT=16 MIN_EXIT_COOKIES=6 MAX_IPC_WAIT_P95_MS=16 MAX_IPC_WAIT_P99_MS=16 MAX_IPC_SCAN_AVG_STEPS_X100=250 ./test_hello.sh
 else
     run_case "m1_recv" no_build env MARKER_MODE=m1_recv TEST_COMMAND_REPEAT=3 MIN_EXIT_COOKIES=3 ./test_hello.sh
     run_case "m2_token_audit" no_build env MARKER_MODE=m2_token_audit TEST_COMMAND_REPEAT=3 MIN_EXIT_COOKIES=3 ./test_hello.sh
@@ -49,7 +49,7 @@ else
     run_case "m4_notify_lifecycle" no_build env MARKER_MODE=m4_notify_lifecycle TEST_COMMAND_REPEAT=1 ./test_hello.sh
     run_case "m4_deny_paths" full env MARKER_MODE=m4_deny_paths TEST_COMMAND_REPEAT=1 RUN_WAIT=12 ./test_hello.sh
     run_case "m4_registry_deny_paths" full env MARKER_MODE=m4_registry_deny_paths TEST_COMMAND_REPEAT=1 RUN_WAIT=12 ./test_hello.sh
-    run_case "m5_fairness" full env MARKER_MODE=m5_fairness TEST_COMMAND_REPEAT=1 RUN_WAIT=16 MIN_EXIT_COOKIES=6 ./test_hello.sh
+    run_case "m5_fairness" full env MARKER_MODE=m5_fairness TEST_COMMAND_REPEAT=1 RUN_WAIT=16 MIN_EXIT_COOKIES=6 MAX_IPC_WAIT_P95_MS=16 MAX_IPC_WAIT_P99_MS=16 MAX_IPC_SCAN_AVG_STEPS_X100=250 ./test_hello.sh
 fi
 
 echo "All harness matrix cases passed."
