76f95d870364299fa21872e1073bf5e21690b23b
Balazs Valkony (XC/ENG4-Bp) <balazs.valkony@hu.bosch.com>
2026-05-07 07:16:58 +0200

    virtio-core: DmaPool fixed-region pinned allocator

--- userspace/virtio-core/src/lib.rs
+++ userspace/virtio-core/src/lib.rs
@@ -10,7 +10,7 @@ pub mod irq;
 pub mod pci;
 
 // re-exports filled in by later tasks
-// pub use dma::{DmaPool, DmaRegion};
+pub use dma::{DmaPool, DmaRegion};
 // pub use virtqueue::{DescChain, Virtqueue};
 // pub use transport::{Transport, FeatureBits};
 // pub use irq::IrqSource;
